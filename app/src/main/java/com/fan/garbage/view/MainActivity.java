68743775b1a59d5b65a876afb8cc0a47763ff310
AFoolsLove <552310594@qq.com>
2018-08-03 22:30:28 +0800

    Optimization

--- app/src/main/java/com/fan/garbage/view/MainActivity.java
+++ app/src/main/java/com/fan/garbage/view/MainActivity.java
@@ -1,18 +1,12 @@
 package com.fan.garbage.view;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.fan.garbage.R;
 import com.fan.garbage.adpater.ViewPagerAdapter;
-import com.fan.garbage.data.BmobDataObject;
-import com.fan.garbage.data.BmobDataSet;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -20,11 +14,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 import cn.bmob.v3.Bmob;
-import cn.bmob.v3.exception.BmobException;
-import cn.bmob.v3.listener.SaveListener;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -36,25 +27,17 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         initView();
         initData();
-//
-//        findViewById(R.id._SSS).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                System.out.println(); // dian
-//            }
-//        });
     }
 
     public void initView() {
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         navigationView.setItemIconTintList(null);
 //Tablayout about
@@ -63,29 +46,29 @@ public class MainActivity extends AppCompatActivity
         tabLayout.setupWithViewPager(viewPager);
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(adapter);
-        TabLayout.Tab o = tabLayout.getTabAt(0);
-        TabLayout.Tab oo = tabLayout.getTabAt(1);
-        TabLayout.Tab ooo = tabLayout.getTabAt(2);
-        TabLayout.Tab oooo = tabLayout.getTabAt(3);
-        assert o != null;
-        o.setIcon(R.drawable.ic_flag);
-        assert oo != null;
-        oo.setIcon(R.drawable.ic_package_variant);
-        assert ooo != null;
-        ooo.setIcon(R.drawable.ic_hangouts);
-        assert oooo != null;
-        oooo.setIcon(R.drawable.ic_leaf);
+//        TabLayout.Tab o = tabLayout.getTabAt(0);
+//        TabLayout.Tab oo = tabLayout.getTabAt(1);
+//        TabLayout.Tab ooo = tabLayout.getTabAt(2);
+//        TabLayout.Tab oooo = tabLayout.getTabAt(3);
+//        o.setIcon(R.drawable.ic_flag_white_24dp);
+//        oo.setIcon(R.drawable.ic_package_variant);
+//        ooo.setIcon(R.drawable.ic_hangouts);
+//        oooo.setIcon(R.drawable.ic_leaf);
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_flag);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_package_variant);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_hangouts);
+        tabLayout.getTabAt(3).setIcon(R.drawable.ic_leaf);
     }
 
     public void initData() {
         Bmob.initialize(this, getString(R.string.bmob_id));
-//        new BmobDataSet().DataSet("test", "test", "test", "test", "test","10",1);
+//        BmobDataSet.dataSet("test", "test", "test", "test", "test","10",1);
     }
 
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -134,7 +117,7 @@ public class MainActivity extends AppCompatActivity
 
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
