160add6e99bfcd8af7cbe2767cac6696a1706ef2
AFoolsLove <552310594@qq.com>
2018-08-04 15:43:12 +0800

    fix RecyclerView item click event

---
+++ app/src/main/java/com/fan/garbage/adpater/OnRecyclerViewClick.java
@@ -0,0 +1,7 @@
+package com.fan.garbage.adpater;
+
+import android.view.View;
+
+public interface OnRecyclerViewClick {
+    void onClick(View view, int position);
+}
--- app/src/main/java/com/fan/garbage/adpater/RecyclerViewAdapter.java
+++ app/src/main/java/com/fan/garbage/adpater/RecyclerViewAdapter.java
@@ -16,80 +16,38 @@ import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import static com.fan.garbage.data.ImgLoad.loadPic;
 
-
-public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.mViewHolder> implements View.OnClickListener {
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.mViewHolder> implements OnRecyclerViewClick {
     private final Context context;
     private ArrayList<Data> data;
-    private OnRecyclerViewClick onRecyclerViewClick = null;
 
     public RecyclerViewAdapter(final Context context, final ArrayList<Data> data) {
         this.context = context;
         this.data = data;
-        //设置点击事件
-        setOnRecyclerViewClick(new RecyclerViewAdapter.OnRecyclerViewClick() {
-            @Override
-            public void itemClick(View view, int position) {
-                Snackbar.make(view, "你点击了布局" + position, Snackbar.LENGTH_SHORT).show();
-            }
+    }
 
-            @Override
-            public void buttonClick(View view, int position) {
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse(data.get(position).url));
-                context.startActivity(intent);
-                Snackbar.make(view, "选择一个东西打开这个链接吧(｡･∀･)ﾉﾞ" + position, Snackbar.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void picClick(View view, int position) {
-                ImageView pic = new ImageView(context);
-                loadPic(context, pic, data.get(position).pic);
-                new AlertDialog.Builder(context)
-                        .setView(pic)
-                        .show();
-            }
-        });
+    @NonNull
+    @Override
+    public mViewHolder onCreateViewHolder(@NonNull final ViewGroup parent, int viewType) {
+        return new mViewHolder(LayoutInflater.from(context).inflate(R.layout.item, parent, false));
     }
 
     @Override
-    public mViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.item, parent, false);
-        MaterialButton button = view.findViewById(R.id.item_money);
-        ImageView imageView = view.findViewById(R.id.item_pic);
-        button.setOnClickListener(this);
-        view.setOnClickListener(this);
-        imageView.setOnClickListener(this);
-        return new mViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(mViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull mViewHolder holder, int position) {
 //        if (contents.isEmpty()) return;
         Data data = this.data.get(position);
-        loadPic(context, holder.pic, data.pic);
-        holder.content.setText(data.content);
-        holder.title.setText(data.title);
-        holder.person.setText(data.bs);
-        holder.money.setText(data.money);
-        holder.money.setTag(position);
-        holder.itemView.setTag(position);
-//        holder.pic.setTag(position);
-//        holder.pic.setTag(position);
-
-    }
-
-    public interface OnRecyclerViewClick {
-        void itemClick(View view, int p);
-
-        void buttonClick(View view, int p);
-
-        void picClick(View view, int p);
-
+        if (data != null) {
+            loadPic(context, holder.pic, data.pic);
+            holder.content.setText(data.content);
+            holder.title.setText(data.title);
+            holder.person.setText(data.bs);
+            holder.money.setText(data.money);
+        }
     }
 
     @Override
@@ -97,21 +55,22 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         return data.size();
     }
 
-    private void setOnRecyclerViewClick(OnRecyclerViewClick onRecyclerViewClick) {
-        this.onRecyclerViewClick = onRecyclerViewClick;
-    }
-
     @Override
-    public void onClick(View view) {
-        if (onRecyclerViewClick != null) {
-            if (view.getId() == R.id.item_money) {
-                onRecyclerViewClick.buttonClick(view, (int) view.getTag());
+    public void onClick(View view, int position) {
+        if (view.getId() == R.id.item_money) {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(data.get(position).url));
+            context.startActivity(intent);
+            Snackbar.make(view, "选择一个东西打开这个链接吧(｡･∀･)ﾉﾞ" + position, Snackbar.LENGTH_SHORT).show();
+        } else {
+            if (view.getId() == R.id.item_pic) {
+                ImageView pic = new ImageView(context);
+                loadPic(context, pic, data.get(position).pic);
+                new AlertDialog.Builder(context)
+                        .setView(pic)
+                        .show();
             } else {
-                if (view.getId() == R.id.item_pic) {
-                    onRecyclerViewClick.picClick(view, (int) view.getTag());
-                } else {
-                    onRecyclerViewClick.itemClick(view, (int) view.getTag());
-                }
+                Snackbar.make(view, "你点击了布局" + position, Snackbar.LENGTH_SHORT).show();
             }
         }
     }
@@ -124,8 +83,19 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
         final TextView person = itemView.findViewById(R.id.item_person);
 
+        final View.OnClickListener itemClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RecyclerViewAdapter.this.onClick(view, getAdapterPosition());
+            }
+        };
+
         mViewHolder(View itemView) {
             super(itemView);
+
+            itemView.setOnClickListener(itemClickListener);
+            money.setOnClickListener(itemClickListener);
+            pic.setOnClickListener(itemClickListener);
         }
     }
 }
